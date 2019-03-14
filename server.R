ad65156784fd22039b08266bb7096d662d66fd32
Cameron Astor <cameron.astor@gmail.com>
2019-03-13 19:33:55 -0700

    food security section

--- server.R
+++ server.R
@@ -1,15 +1,19 @@
 library("shiny")
 library("dplyr")
 library("leaflet")
+library("stringr")
+library("rbokeh")
 library("tidyr")
 library("ggplot2")
 library("ggmap")
+
 # LOAD ALL DATA SETS HERE AT THE BEGINNING
 countries <- read.csv("data/countries_long_lat.csv", stringsAsFactors = FALSE)
 full_pop_data <- read.csv("data/FAOSTAT_population.csv", stringsAsFactors = FALSE)
 full_land_data <- read.csv("data/FAOSTAT_landuse.csv", stringsAsFactors = FALSE)
 df <- read.csv("https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv", stringsAsFactors = FALSE)
-
+food_data <- read.csv("data/FAOSTAT_foodsecurity.csv", stringsAsFactors = FALSE)
+country_region <- read.csv("data/country_region.csv", stringsAsFactors = FALSE)
 
 my_server <- function(input, output) {
   #--------------------------------------------CHOROPLETH PAGE------------------------------------------------------------------------------------------------- 
@@ -113,7 +117,60 @@ my_server <- function(input, output) {
   #--------------------------------------------END MACROINDICATOR PAGE----------------------------------------------------------------------------------------
 
   #--------------------------------------------FOOD-SECURITY PAGE--------------------------------------------------------------------------------------------- 
-
+  
+  #clean up region data
+  country_region <- country_region %>%
+    select(name, region)
+  names(country_region) <- c("Area", "region")
+  
+  # Try to make something of this mess
+  item_by_country <- reactive({
+    item_by_country <- clean_food_data %>%
+      # spread(key = Item, value = Value) %>%
+      group_by(Area) %>%
+      filter(Item == input$indicator) %>%
+      filter(str_detect(Year, input$year)) %>%
+      ungroup()
+    item_by_country$Value <- as.double(item_by_country$Value)
+    item_by_country <- item_by_country %>%
+      left_join(country_region)
+    item_by_country
+  })
+  
+  # Create the dropdown for years
+  output$year_selector <- renderUI({
+    
+    # Create the reactive values for the year dropdown
+    years_available <- reactive({
+      years_available <- clean_food_data %>%
+        filter(Item == input$indicator, Area == "Afghanistan") %>%
+        select(Year)
+      year_vector <- pull(years_available)
+      year_vector
+    })
+    
+    # Create the dropdown itself
+    selectInput(
+      inputId = "year",
+      label = "Year",
+      choices = years_available(),
+      selected = years_available()[1]
+    )
+  })
+  
+  # Create the plot
+  output$plot <- renderRbokeh({
+    p <- figure() %>%
+      ly_points(
+        Area, Value,
+        data = item_by_country(),
+        color = region,
+        hover = list(Area, Value)
+      ) %>%
+      y_axis(label = input$indicator) %>%
+      x_axis(visible = FALSE)
+    p
+  })
 
 
   #--------------------------------------------END FOOD-SECURITY PAGE-----------------------------------------------------------------------------------------
--- ui.R
+++ ui.R
@@ -177,7 +177,49 @@ about if there is a correlation between these domains."),
     )
   ),
   tabPanel(
-    "Food Security"
+    "Food Security",
+    titlePanel("Food Security Indicators"),
+    sidebarLayout(
+      sidebarPanel(
+        selectInput(
+          "indicator",
+          "Food Security Indicator",
+          choices = item_names
+        ),
+        htmlOutput("year_selector"),
+        p(strong("Indicators")),
+        
+        # Indicator information
+        p(strong("Average dietary energy supply adequacy")),
+        p("The indicator expresses the Dietary Energy Supply (DES)
+          as a percentage of the Average Dietary Energy Requirement (ADER)."),
+        
+        p(strong("Number of people undernourished")),
+        p("Estimated number of people at risk of undernourishment.
+          It is calculated by applying the estimated prevalence of
+          undernourishment to total population in each period."),
+        
+        p(strong("Percent of arable land equipped for irrigation")),
+        p("Ratio between arable land equipped for irrigation and total arable land."),
+        
+        p(strong("Value of food imports in total merchandise exports")),
+        p("Value of food (excl. fish) imports over total merchandise exports."),
+        
+        p(strong("Percentage of population using at least basic drinking
+                 water services")),
+        p("See above."),
+        
+        p(strong("Percentage of population using at least basic sanitation services")),
+        p("See above."),
+        
+        p(strong("Prevalence of obesity in the adult population.")),
+        p("(Adult defined as over 18 years of age)")
+        ),
+      mainPanel(
+        p("Food Security Indicators by Country"),
+        rbokehOutput("plot")
+      )
+        )
   ),
   tabPanel(
     "Energy Use",
